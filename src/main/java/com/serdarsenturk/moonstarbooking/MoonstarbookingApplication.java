33b9b961a618042395e87df4b7a278057190dbbb
Serdar <serdarsenturk@windowslive.con>
2021-06-07 20:21:54 +0300

    First commit

---
+++ src/main/java/com/serdarsenturk/moonstarbooking/MoonstarbookingApplication.java
@@ -0,0 +1,13 @@
+package com.serdarsenturk.moonstarbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonstarbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonstarbookingApplication.class, args);
+	}
+
+}
