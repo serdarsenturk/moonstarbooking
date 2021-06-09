580195ba25e02da173b6f80b768a2e884d292e3f
Serdar <serdarsenturk@windowslive.con>
2021-06-09 22:25:42 +0300

    Remove findAll and findCompanyByName methods because these are built-in function

--- src/main/java/com/serdarsenturk/moonstarbooking/CompanyRepository.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/CompanyRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CompanyRepository extends JpaRepository<Company, String> {
-        List<Company> findAll();
-
-        List<Company> findCompanyByName(String name);
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+        List<Company> findByNameStartsWithIgnoreCase(String name);
 }
