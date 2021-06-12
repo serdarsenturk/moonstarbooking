eea94204aaa83715b1139e6ef6e3d6a277e24b92
Serdar <serdarsenturk@windowslive.con>
2021-06-13 01:00:32 +0300

    Create AdminView and theirs methods

---
+++ src/main/java/com/serdarsenturk/moonstarbooking/views/admin/AdminView.java
@@ -0,0 +1,90 @@
+package com.serdarsenturk.moonstarbooking.views.admin;
+
+import com.serdarsenturk.moonstarbooking.views.company.CompanyView;
+import com.serdarsenturk.moonstarbooking.views.home.HomeView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.TabVariant;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+import java.util.Optional;
+
+@Route(value = "admin")
+@PageTitle("Admin")
+public class AdminView extends AppLayout {
+    private final Tabs menu;
+
+    public AdminView(){
+        HorizontalLayout header = createHeader();
+        menu = createMenuTabs();
+        addToNavbar(createTopBar(header, menu));
+    }
+
+    private VerticalLayout createTopBar(HorizontalLayout header, Tabs menu) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.getThemeList().add("dark");
+        layout.setWidthFull();
+        layout.setSpacing(false);
+        layout.setPadding(false);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.add(header, menu);
+        return layout;
+    }
+
+    private HorizontalLayout createHeader() {
+        HorizontalLayout header = new HorizontalLayout();
+        header.setClassName("topmenu-header");
+        header.setPadding(false);
+        header.setSpacing(false);
+        header.setWidthFull();
+        header.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        Image logo = new Image("images/logo.png", "as logo");
+        logo.setId("logo");
+        header.add(logo);
+        header.add(new H1("Admin"));
+        return header;
+    }
+
+    private static Tabs createMenuTabs() {
+        final Tabs tabs = new Tabs();
+        tabs.getStyle().set("max-width", "100%");
+        tabs.add(getAvailableTabs());
+        return tabs;
+    }
+
+    private static Tab[] getAvailableTabs() {
+        return new Tab[]{createTab("Home", HomeView.class), createTab("Companies", CompanyView.class)};
+    }
+
+    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
+        final Tab tab = new Tab();
+        tab.addThemeVariants(TabVariant.LUMO_ICON_ON_TOP);
+        tab.add(new RouterLink(text, navigationTarget));
+        ComponentUtil.setData(tab, Class.class, navigationTarget);
+        return tab;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+    }
+
+    private Optional<Tab> getTabForComponent(Component component) {
+        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
+                .findFirst().map(Tab.class::cast);
+    }
+
+}
+
