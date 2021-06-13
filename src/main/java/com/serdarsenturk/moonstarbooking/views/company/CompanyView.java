940cdc05b4de64c32d4e857053e6516b90af6151
Serdar <serdarsenturk@windowslive.con>
2021-06-14 00:44:24 +0300

    Create CompanyView

---
+++ src/main/java/com/serdarsenturk/moonstarbooking/views/company/CompanyView.java
@@ -0,0 +1,81 @@
+package com.serdarsenturk.moonstarbooking.views.company;
+
+import com.serdarsenturk.moonstarbooking.data.entity.Company;
+import com.serdarsenturk.moonstarbooking.data.service.CompanyEditor;
+import com.serdarsenturk.moonstarbooking.data.repository.ICompanyRepository;
+import com.serdarsenturk.moonstarbooking.views.admin.AdminView;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
+
+@Route(value = "admin/companies", layout = AdminView.class)
+@PageTitle("Companies")
+public class CompanyView extends VerticalLayout {
+
+    private final ICompanyRepository repo;
+
+    private final CompanyEditor editor;
+
+    final Grid<Company> grid;
+
+    final TextField filter;
+
+    final Select<String> labelSelect;
+
+    private final Button addNewBtn;
+
+    public CompanyView(ICompanyRepository repo, CompanyEditor editor){
+        this.repo = repo;
+        this.editor = editor;
+        this.grid = new Grid<>(Company.class);
+        this.filter = new TextField();
+        this.addNewBtn = new Button("New Company", VaadinIcon.PLUS.create());
+        this.labelSelect = new Select<>();
+
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn, labelSelect);
+        add(actions, grid, editor);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "name", "owner");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
+        filter.setPlaceholder("Filter by name");
+
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> listCompanies(e.getValue()));
+
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            editor.editCompany(e.getValue());
+        });
+
+        addNewBtn.addClickListener(e -> editor.editCompany(new Company("", "")));
+
+        labelSelect.setItems("Option one", "Option two");
+        labelSelect.setLabel("Label");
+
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listCompanies(filter.getValue());
+        });
+
+        listCompanies(null);
+    }
+
+    void listCompanies(String filterText){
+        if (StringUtils.isEmpty(filterText)) {
+            grid.setItems(repo.findAll());
+        }
+        else {
+            grid.setItems(repo.findByNameStartsWithIgnoreCase(filterText));
+        }
+    }
+
+}
