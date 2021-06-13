7a1d85dce844a483333980204d137157068a6375
Serdar <serdarsenturk@windowslive.con>
2021-06-14 00:43:25 +0300

    Create IPassengerRepository

---
+++ src/main/java/com/serdarsenturk/moonstarbooking/data/repository/IPassengerRepository.java
@@ -0,0 +1,11 @@
+package com.serdarsenturk.moonstarbooking.data.repository;
+
+import com.serdarsenturk.moonstarbooking.data.entity.Company;
+import com.serdarsenturk.moonstarbooking.data.entity.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IPassengerRepository extends JpaRepository<Passenger, Integer> {
+    List<Passenger> findByFirstNameStartsWithIgnoreCase(String firstName);
+}
