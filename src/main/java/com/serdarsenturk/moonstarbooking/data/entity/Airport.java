5d597a45ffff225ead71e77b307915ff2ae279e6
Serdar <serdarsenturk@windowslive.con>
2021-06-20 18:31:49 +0300

    Create Airport entity with methods and properties

---
+++ src/main/java/com/serdarsenturk/moonstarbooking/data/entity/Airport.java
@@ -0,0 +1,30 @@
+package com.serdarsenturk.moonstarbooking.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    public Airport(){}
+
+    public Airport(String name){
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
