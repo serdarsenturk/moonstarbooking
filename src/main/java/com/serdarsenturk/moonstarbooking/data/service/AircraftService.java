ad2bff46df412743330219d725edf189fd72817d
Serdar <serdarsenturk@windowslive.con>
2021-06-24 03:33:35 +0300

    Update AircraftService

--- src/main/java/com/serdarsenturk/moonstarbooking/data/service/AircraftService.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/data/service/AircraftService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vaadin.artur.helpers.CrudService;
 
-import java.time.LocalDate;
-
 @Service
 public class AircraftService extends CrudService<Aircraft, Integer> {
     private final IAircraftRepository repository;
