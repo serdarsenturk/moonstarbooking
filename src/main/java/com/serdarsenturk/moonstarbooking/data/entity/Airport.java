032a1172604c355e7727bcab32df10b8c5f434b9
Serdar <serdarsenturk@windowslive.con>
2021-06-24 03:33:06 +0300

    Remove useless packages

--- src/main/java/com/serdarsenturk/moonstarbooking/data/entity/Airport.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/data/entity/Airport.java
@@ -1,9 +1,8 @@
 package com.serdarsenturk.moonstarbooking.data.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Airport {
@@ -20,6 +19,7 @@ public class Airport {
         this.name = name;
     }
 
+
     public Integer getId() {
         return id;
     }
@@ -31,5 +31,5 @@ public class Airport {
     public void setName(String name) {
         this.name = name;
     }
-    
+
 }
