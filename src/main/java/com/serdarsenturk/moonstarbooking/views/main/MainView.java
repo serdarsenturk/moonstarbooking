4c7aac66b1ee6a4a0aa59e3dca9e341b7ac3f3a0
Serdar <serdarsenturk@windowslive.con>
2021-06-26 16:28:38 +0300

    Change sidebar menu to topbar menu

--- src/main/java/com/serdarsenturk/moonstarbooking/views/main/MainView.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/views/main/MainView.java
@@ -2,120 +2,32 @@ package com.serdarsenturk.moonstarbooking.views.main;
 
 import com.serdarsenturk.moonstarbooking.views.admin.AdminView;
 import com.serdarsenturk.moonstarbooking.views.home.HomeView;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.component.tabs.TabsVariant;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLink;
 
-import java.util.Optional;
-
 public class MainView extends AppLayout {
-    private final Tabs menu;
-    private H1 viewTitle;
-
     public MainView() {
-        // Use the drawer for the menu
-        setPrimarySection(Section.DRAWER);
+        H1 title = new H1("Moonstar Booking");
+        title.getStyle()
+                .set("font-size", "var(--lumo-font-size-l)")
+                .set("left", "var(--lumo-space-l)")
+                .set("margin", "0")
+                .set("position", "absolute");
 
-        // Make the nav bar a header
-        addToNavbar(true, createHeaderContent());
+        Tabs tabs = new Tabs();
+        FlexLayout centeredLayout = new FlexLayout();
+        centeredLayout.setSizeFull();
+        centeredLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        centeredLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        centeredLayout.add(tabs);
 
-        // Put the menu in the drawer
-        menu = createMenu();
-        addToDrawer(createDrawerContent(menu));
+        tabs.add(new Tab(new RouterLink("Home", HomeView.class)));
+        tabs.add(new Tab(new RouterLink("Admin", AdminView.class)));
+        addToNavbar(false, centeredLayout, title);
     }
-
-    private Component createHeaderContent() {
-        HorizontalLayout layout = new HorizontalLayout();
-
-        // Configure styling for the header
-        layout.setId("header");
-        layout.getThemeList().set("dark", true);
-        layout.setWidthFull();
-        layout.setSpacing(false);
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        // Have the drawer toggle button on the left
-        layout.add(new DrawerToggle());
-
-        // Placeholder for the title of the current view.
-        // The title will be set after navigation.
-        viewTitle = new H1();
-        layout.add(viewTitle);
-
-        return layout;
-    }
-
-    private Component createDrawerContent(Tabs menu) {
-        VerticalLayout layout = new VerticalLayout();
-
-        // Configure styling for the drawer
-        layout.setSizeFull();
-        layout.setPadding(false);
-        layout.setSpacing(false);
-        layout.getThemeList().set("spacing-s", true);
-        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-
-        // Have a drawer header with an application logo
-        HorizontalLayout logoLayout = new HorizontalLayout();
-        logoLayout.setId("logo");
-        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        logoLayout.add(new H1("Moonstar Booking"));
-
-        // Display the logo and the menu in the drawer
-        layout.add(logoLayout, menu);
-        return layout;
-    }
-
-    private Tabs createMenu() {
-        final Tabs tabs = new Tabs();
-        tabs.setOrientation(Tabs.Orientation.HORIZONTAL);
-        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
-        tabs.setId("tabs");
-        tabs.add(createMenuItems());
-        return tabs;
-    }
-
-    private Component[] createMenuItems() {
-        return new Tab[]{
-                createTab("Admin", AdminView.class),
-                createTab("Home", HomeView.class)};
-    }
-
-    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
-        final Tab tab = new Tab();
-        tab.add(new RouterLink(text, navigationTarget));
-        ComponentUtil.setData(tab, Class.class, navigationTarget);
-        return tab;
-    }
-
-    @Override
-    protected void afterNavigation() {
-        super.afterNavigation();
-
-        // Select the tab corresponding to currently shown view
-        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
-
-        // Set the view title in the header
-        viewTitle.setText(getCurrentPageTitle());
-    }
-
-    private Optional<Tab> getTabForComponent(Component component) {
-        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
-                .findFirst().map(Tab.class::cast);
-    }
-
-    private String getCurrentPageTitle() {
-        return getContent().getClass().getAnnotation(PageTitle.class).value();
-    }
-
 }
