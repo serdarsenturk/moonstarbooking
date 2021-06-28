75d35720c24a94f1b751bba894199f83ccad781f
Serdar <serdarsenturk@windowslive.con>
2021-06-28 15:12:20 +0300

    Update IFlightRepository

--- src/main/java/com/serdarsenturk/moonstarbooking/data/repository/IFlightRepository.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/data/repository/IFlightRepository.java
@@ -1,8 +1,13 @@
 package com.serdarsenturk.moonstarbooking.data.repository;
 
+import com.serdarsenturk.moonstarbooking.data.entity.Airport;
 import com.serdarsenturk.moonstarbooking.data.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface IFlightRepository extends JpaRepository<Flight, Integer> {
-    
+    List<Flight> findFlightByFromAirportAndToAirportAndDepartureDate(@Param("from") Airport from , @Param("to") Airport to, @Param("date") LocalDate date);
 }
