315782b3583607aab81ac532c3f0c9a5dc8b9630
Serdar <serdarsenturk@windowslive.con>
2021-06-29 01:10:55 +0300

    Update MoonstarbookingApplication extend class and tags

--- src/main/java/com/serdarsenturk/moonstarbooking/MoonstarbookingApplication.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/MoonstarbookingApplication.java
@@ -1,13 +1,21 @@
 package com.serdarsenturk.moonstarbooking;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+import com.vaadin.flow.theme.Theme;
 
 @SpringBootApplication
-public class MoonstarbookingApplication {
+@Theme(value = "moonstarbooking")
+@PWA(name = "moonstarbooking", shortName = "moonstarbooking", offlineResources = {"images/logo.png"})
+public class MoonstarbookingApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoonstarbookingApplication.class, args);
+		LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(MoonstarbookingApplication.class, args));
 	}
 
 }
