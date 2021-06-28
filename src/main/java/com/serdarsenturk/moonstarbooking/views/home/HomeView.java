7805042544df45ed3af2c75d8d58b0c36c08b45d
Serdar <serdarsenturk@windowslive.con>
2021-06-28 13:00:44 +0300

    Create HomeView

--- src/main/java/com/serdarsenturk/moonstarbooking/views/home/HomeView.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/views/home/HomeView.java
@@ -1,29 +1,112 @@
 package com.serdarsenturk.moonstarbooking.views.home;
 
+import com.serdarsenturk.moonstarbooking.data.entity.Airport;
+import com.serdarsenturk.moonstarbooking.data.entity.Flight;
+import com.serdarsenturk.moonstarbooking.data.repository.IAirportRepository;
+import com.serdarsenturk.moonstarbooking.data.service.FlightService;
 import com.serdarsenturk.moonstarbooking.views.main.MainView;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
 
 @Route(value = "/", layout = MainView.class)
-@PageTitle("Hello World")
-public class HomeView extends HorizontalLayout {
+@PageTitle("Moonstar Booking")
+public class HomeView extends Div {
 
-    private TextField name;
-    private Button sayHello;
+    private ComboBox<Airport> fromAirport = new ComboBox<>("FROM");
+    private ComboBox<Airport> toAirport = new ComboBox<>("TO");
 
-    public HomeView(){
-        setId("home-view");
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        add(name, sayHello);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+    private DatePicker date = new DatePicker("Date");
+
+    private Button search = new Button("Search");
+
+    private Binder<Flight> binder;
+
+    private IAirportRepository repository;
+
+    private FlightService flightService;
+
+    Grid<Flight> grid = new Grid<>(Flight.class, false);
+
+    public HomeView(@Autowired FlightService flightService, IAirportRepository repository){
+        this.repository = repository;
+        this.flightService = flightService;
+
+        binder = new Binder<>(Flight.class);
+
+        SplitLayout splitLayout = new SplitLayout();
+        splitLayout.setSizeFull();
+
+        createGridLayout(splitLayout);
+
+        add(createTitle());
+        add(createFormLayout());
+        add(createButtonLayout());
+        add(splitLayout);
+
+        binder.bindInstanceFields(this);
+
+        grid.addColumn("flightCode").setAutoWidth(true);
+        grid.addColumn("toAirportName").setAutoWidth(true);
+        grid.addColumn("fromAirportName").setAutoWidth(true);
+        grid.addColumn("departureDate").setAutoWidth(true);
+        grid.addColumn("arrivalDate").setAutoWidth(true);
+        grid.addColumn("cost").setAutoWidth(true);
+
+//        clearForm();
     }
 
+    private Component createTitle() {
+        return new H3("Search Flight");
+    }
+
+    private Component createFormLayout() {
+        FormLayout formLayout = new FormLayout();
+
+        fromAirport.setWidth("50%");
+        fromAirport.setItems(repository.findAll());
+        fromAirport.setItemLabelGenerator(Airport::getName);
+
+        toAirport.setWidth("50%");
+        toAirport.setItems(repository.findAll());
+        toAirport.setItemLabelGenerator(Airport::getName);
+
+        date.setWidth("50%");
+
+        formLayout.add(fromAirport, toAirport, date);
+        return formLayout;
+    }
+
+    private Component createButtonLayout() {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.addClassName("button-layout");
+        search.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonLayout.add(search);
+        return buttonLayout;
+    }
+
+    private void createGridLayout(SplitLayout splitLayout) {
+        Div wrapper = new Div();
+        wrapper.setId("grid-wrapper");
+        wrapper.setWidthFull();
+        splitLayout.addToPrimary(wrapper);
+        wrapper.add(grid);
+    }
+    
 }
