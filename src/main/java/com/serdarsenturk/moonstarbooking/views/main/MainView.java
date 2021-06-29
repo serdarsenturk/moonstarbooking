769cd09983a3bed9708d71ffd80f5db266f4c475
Serdar <serdarsenturk@windowslive.con>
2021-06-29 13:12:59 +0300

    Update MainView with Header

--- src/main/java/com/serdarsenturk/moonstarbooking/views/main/MainView.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/views/main/MainView.java
@@ -2,32 +2,86 @@ package com.serdarsenturk.moonstarbooking.views.main;
 
 import com.serdarsenturk.moonstarbooking.views.admin.AdminView;
 import com.serdarsenturk.moonstarbooking.views.home.HomeView;
+import java.util.Optional;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.TabVariant;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
 
 public class MainView extends AppLayout {
+
+        private final Tabs menu;
+
     public MainView() {
-        H1 title = new H1("Moonstar Booking");
-        title.getStyle()
-                .set("font-size", "var(--lumo-font-size-l)")
-                .set("left", "var(--lumo-space-l)")
-                .set("margin", "0")
-                .set("position", "absolute");
+            HorizontalLayout header = createHeader();
+            menu = createMenuTabs();
+            addToNavbar(createTopBar(header, menu));
+        }
 
-        Tabs tabs = new Tabs();
-        FlexLayout centeredLayout = new FlexLayout();
-        centeredLayout.setSizeFull();
-        centeredLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        centeredLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        centeredLayout.add(tabs);
+        private VerticalLayout createTopBar(HorizontalLayout header, Tabs menu) {
+            VerticalLayout layout = new VerticalLayout();
+            layout.getThemeList().add("light");
+            layout.setWidthFull();
+            layout.setSpacing(false);
+            layout.setPadding(false);
+            layout.setAlignItems(FlexComponent.Alignment.CENTER);
+            layout.add(header, menu);
+            return layout;
+        }
 
-        tabs.add(new Tab(new RouterLink("Home", HomeView.class)));
-        tabs.add(new Tab(new RouterLink("Admin", AdminView.class)));
-        addToNavbar(false, centeredLayout, title);
-    }
+        private HorizontalLayout createHeader() {
+            HorizontalLayout header = new HorizontalLayout();
+            header.setClassName("topmenu-header");
+            header.setPadding(false);
+            header.setSpacing(false);
+            header.setWidthFull();
+            header.setAlignItems(FlexComponent.Alignment.CENTER);
+            Image logo = new Image("images/logo.png", "Moonstar");
+            logo.setId("logo");
+            header.add(logo);
+            header.add(new H1("Moonstar Booking"));
+
+            return header;
+        }
+
+        private static Tabs createMenuTabs() {
+            final Tabs tabs = new Tabs();
+            tabs.getStyle().set("max-width", "100%");
+            tabs.add(getAvailableTabs());
+            return tabs;
+        }
+
+        private static Tab[] getAvailableTabs() {
+            return new Tab[]{
+                    createTab("Home", HomeView.class),
+                    createTab("Admin", AdminView.class)};
+        }
+
+        private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
+            final Tab tab = new Tab();
+            tab.addThemeVariants(TabVariant.LUMO_ICON_ON_TOP);
+            tab.add(new RouterLink(text, navigationTarget));
+            ComponentUtil.setData(tab, Class.class, navigationTarget);
+            return tab;
+        }
+
+        @Override
+        protected void afterNavigation() {
+            super.afterNavigation();
+            getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+        }
+
+        private Optional<Tab> getTabForComponent(Component component) {
+            return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
+                    .findFirst().map(Tab.class::cast);
+        }
+
 }
