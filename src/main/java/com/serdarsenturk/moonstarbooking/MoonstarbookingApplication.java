0dc087a8321c5727859644fa9ab73e8a591d3772
Serdar <serdarsenturk@windowslive.con>
2021-06-30 10:49:29 +0300

    Update Theme value

--- src/main/java/com/serdarsenturk/moonstarbooking/MoonstarbookingApplication.java
+++ src/main/java/com/serdarsenturk/moonstarbooking/MoonstarbookingApplication.java
@@ -3,6 +3,7 @@ package com.serdarsenturk.moonstarbooking;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 
+import com.vaadin.flow.theme.material.Material;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,8 +11,8 @@ import org.vaadin.artur.helpers.LaunchUtil;
 import com.vaadin.flow.theme.Theme;
 
 @SpringBootApplication
-@Theme(value = "moonstarbooking")
-@PWA(name = "moonstarbooking", shortName = "moonstarbooking", offlineResources = {"images/logo.png"})
+@Theme(themeClass = Material.class)
+@PWA(name = "moonstarbooking", shortName = "moonstarbooking", offlineResources = {"images/logo1.png"})
 public class MoonstarbookingApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
